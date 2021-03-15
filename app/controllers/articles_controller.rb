a390e844106e128794c95dcc001d01f770d503c3
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-03-15 13:15:10 +0530

    Adds AJAX to create new article from index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,25 @@
 class ArticlesController < ApplicationController
     def index
         @articles = Article.all
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        respond_to do |format|
+            if @article.save
+                @articles = Article.all
+                @article = Article.new
+                format.html { redirect_to articles_path }
+                format.js
+            else
+            end
+        end
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title)
     end
 end
--- spec/features/article_index_page_spec.rb
+++ spec/features/article_index_page_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
-feature "visiting Article Index Page" do
-    scenario "By Any User" do
+feature "Article Index Page" do
+    scenario "List All Articles" do
         create :article
         visit articles_path
         expect(page).to have_content("All Articles")
         expect(page).to have_content("Test Title One")
+    end
+
+    scenario "able to find new article form" do
+        visit articles_path
+
+        expect(page).to have_button "Create Article"
+    end
+
+    scenario "able to create new article from index page and updates index list" do
+        visit articles_path
+
+        fill_in :article_title, with: "Rails AJAX"
+
+        click_button "Create Article"
+
+        expect(page).to have_content "Rails AJAX"
     end
 end
