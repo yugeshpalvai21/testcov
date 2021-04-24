5fb0031e39389c016f632275d8db74eec3e04ded
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-04-24 11:58:34 +0530

    Installs and configures FFaker gem to generate dummy data

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -9,5 +9,8 @@ RSpec.describe "Outer Example Group" do
     it 'test simple calculation of a,b substraction' do
       expect(10-4).to eq 6
     end
+    it 'tests FFAKER DUMMY DATA' do
+      expect(FFaker::Name.first_name).not_to eq "yugesh"
+    end
   end
 end
